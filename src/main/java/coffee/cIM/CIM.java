0088f8a462672fdb3e8c500ab10cf8050c422930
Moura <joaomourabbrandao07@gmail.com>
2026-01-19 14:31:41 -0300

    Uploaded files

---
+++ src/main/java/coffee/cIM/CIM.java
@@ -0,0 +1,17 @@
+package coffee.cIM;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CIM extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
